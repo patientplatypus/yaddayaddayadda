241fe3575435fd261b11de5668ab7af77bac7ddd
Peter Weyadn <patientplatypus@Peters-MacBook-Pro.local>
2019-05-26 14:21:51 -0400

    sick of these drawers

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:yaddayaddayadda/state/yadaState.dart';
 import 'package:yaddayaddayadda/pages/home/home.dart';
-import 'package:yaddayaddayadda/components/header/header.dart';
+// import 'package:yaddayaddayadda/components/header/header.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
--- flutter/lib/pages/home/home.dart
+++ flutter/lib/pages/home/home.dart
@@ -1,12 +1,115 @@
 
 import 'package:flutter/material.dart';
-import 'package:yaddayaddayadda/components/header/header.dart';
-import 'package:yaddayaddayadda/components/footer/footer.dart';
-import 'package:yaddayaddayadda/components/leftMenu/leftMenu.dart';
-import 'package:yaddayaddayadda/components/middle/middlehome.dart';
 
-class Home extends StatelessWidget{
-  
+import 'package:yaddayaddayadda/components/footer/footer.dart';
+import 'package:yaddayaddayadda/components/middle/middlehome.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+import 'package:yaddayaddayadda/state/yadaState.dart';
+import 'package:provider/provider.dart';
+
+
+class MyDrawer extends StatefulWidget {
+  MyDrawer({@required this.menuOpen, @required this.changeMenuState});
+  final bool menuOpen;
+  final void Function(String) changeMenuState;
+
+  @override
+  _MyDrawerState createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
+
+  @override
+  void initState() {
+    super.initState();
+    print('inside initState of MyDrawer');
+    print('and value of widget');
+    print(widget);
+    if(widget.menuOpen==false){
+      this.dispose();
+    }else if(widget.menuOpen==true){
+      print('inside widget.menuOpen in initState');
+      new Future.delayed(Duration.zero,() {
+        print('inside future delayed in initState');
+        Scaffold.of(context).openDrawer();
+      });
+    }
+    print("open");
+  }
+
+  @override
+  void dispose() {
+    print("close");
+    if(widget.menuOpen==true){
+      widget.changeMenuState("close");
+      super.dispose();
+    }else{
+      super.dispose();
+    }
+  }
+
+  // void openDrawer(context){
+  //   print('inside openDrawer function');
+  //   Scaffold.of(context).openDrawer();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    // if(widget.menuOpen==true){
+    //   this.openDrawer(context);
+    // }
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Text("test1"),
+          Text("test2"),
+          Text("test3"),
+        ],
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _Home createState() => _Home();
+}
+
+class _Home extends State<Home>{
+
+  bool menuOpen = false;
+
+  @override
+  void initState() {
+    super.initState();
+    print("open");
+  }
+
+  @override
+  void dispose() {
+    print("close");
+    super.dispose();
+  }
+
+  void changeMenuState(stateVal){
+    print('inside changeMenuState and value of stateVal');
+    print(stateVal);
+    if(stateVal=='na'){
+      setState(() {
+        menuOpen = !menuOpen;
+      });
+    }else if(stateVal=='close'){
+      setState(() {
+        menuOpen = false;
+      });
+    }else if(stateVal=='open'){
+      setState(() {
+        menuOpen = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -16,16 +119,28 @@ class Home extends StatelessWidget{
     double height = MediaQuery.of(context).size.height;
     var orientation = MediaQuery.of(context).orientation;
 
+    GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
+
     if(orientation==Orientation.portrait){
       return Container(
         child: new Scaffold(
           primary: true,
            appBar: new AppBar(
-            // centerTitle: true,
+            automaticallyImplyLeading: false,
             title: new Row(
               mainAxisAlignment: MainAxisAlignment.end,
               mainAxisSize: MainAxisSize.max,
               children: <Widget>[
+                new IconButton(
+                  icon: new Icon(MdiIcons.menu), 
+                  onPressed: (){
+                    if(this.menuOpen==false){
+                      this.changeMenuState('open');
+                    }else{
+                      this.changeMenuState('close');
+                    }
+                  }
+                ),
                 new Text(
                   "YaddaYaddaYadda",
                   style: TextStyle(
@@ -39,21 +154,22 @@ class Home extends StatelessWidget{
             brightness: Brightness.dark,
             backgroundColor: Color.fromRGBO(217, 191, 160, 1),
           ),
-          drawer: new Drawer(
-            child: new Text("\n\n\nDrawer Is Here"),
-          ),
-          body: new Container(
-            child: new Stack(
-              children: <Widget>[
-                new MiddleHome(),
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child: new Footer(),
-                )
-              ],
-            ), 
-            color:  Color.fromRGBO(64, 45, 34, 1),
-            width: width,
+          body: new Scaffold(
+            key: _scaffoldKey,
+            drawer: new MyDrawer(menuOpen: this.menuOpen, changeMenuState: (stateVal)=>this.changeMenuState(stateVal)),
+            body: new Container(
+              child: new Stack(
+                children: <Widget>[
+                  new MiddleHome(),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: new Footer(),
+                  )
+                ],
+              ), 
+              color:  Color.fromRGBO(64, 45, 34, 1),
+              width: width,
+            )
           )
         ),
         width: width, 
@@ -92,7 +208,7 @@ class Home extends StatelessWidget{
                 )
               ],
             ), 
-            color:  Color.fromRGBO(64, 45, 34, 1),
+            color: Color.fromRGBO(64, 45, 34, 1),
             width: width,
           )
         ),
@@ -100,56 +216,5 @@ class Home extends StatelessWidget{
         height: height
       );
     }
-
-
-
-//  new PreferredSize(
-//             preferredSize: Size.fromHeight(50.0), // here the desired height
-//             child: new AppBar(
-//               title: new Text(
-//                 "YaddaYaddaYadda",
-//                 style: TextStyle(
-//                   fontFamily: 'Signpainter',
-//                   fontSize: 20.0,
-//                   color: Color.fromRGBO(140, 91, 48, 1)
-//                 )
-//               ),
-//               backgroundColor: Color.fromRGBO(217, 191, 160, 1),
-//             )
-//           ),
-
-
-
-    // print("Inside home and the value of orientation: ");
-    // print(orientation);
-    // print(orientation==Orientation.portrait);
-
-    // return Scaffold(
-    //   body: Container(
-    //     child: new Stack( 
-    //       children: <Widget>[
-    //         new Positioned(
-    //           top: 0, 
-    //           left: 0,
-    //           child: new Header()
-    //         ), 
-    //         new Positioned(
-    //           top: .1*height, 
-    //           left: 0,
-    //           child: new MiddleHome()
-    //         ), 
-    //         new Positioned(
-    //           top: .95*height, 
-    //           left: 0,
-    //           child: new Footer()
-    //         ),
-    //         // new LeftMenu()
-    //       ],
-    //     ),
-    //     color: Color.fromRGBO(64, 45, 34, 1),
-    //     width: width,
-    //     height: height,
-    //   ) 
-    // );
   }
 }
